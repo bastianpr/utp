1ada1a58feeebd8fcf6209a6c80657da5c4923f1
Bastian <112686261+bastianpr@users.noreply.github.com>
2022-10-15 13:47:16 +0700

    Update distribusihipergeometrik_standar.cpp

--- pertama/distribusihipergeometrik_standar.cpp
+++ pertama/distribusihipergeometrik_standar.cpp
@@ -14,15 +14,15 @@
     {  
     batasan();
     //untuk nilai awal
-       long long x,k,nbesar,nkecil;
+       long long int x,k,nbesar,nkecil;
     //untuk kepentingan menampung nilai
-       long long i, kn = 1, xn = 1, nbn = 1, nkn = 1;
+       long long int i, kn = 1, xn = 1, nbn = 1, nkn = 1;
     //untuk bagian atas sebagai yang dibagi 
     //nb = n besar, nk = n kecil.
-       long long kminx, nbmink, nkminx,  nbminknkminx;
+       long long int kminx, nbmink, nkminx,  nbminknkminx;
     //untuk bagian bawah sebagai pembagi
     //nb = n besar, nk = n kecil. 
-       long long nbminnkz;
+       long long int nbminnkz;
        
     //mulai memasukkan nilai argumen atau nilai awal
     cout<<"Masukkan jumlah populasi atau N (Misalnya 7) : ";
@@ -40,16 +40,9 @@
 	nkminx = nkecil - x;
 	nbminknkminx = nbmink - nkminx;
 	nbminnkz = nbesar - nkecil;
-    
-    //pemberitahuan mengenai nilai yang kemungkinan terlalu banyak sehingga terhitung secara tidak benar
-     if(x < 1 && k < 1 && nkecil < 1 && nbesar < 1){
-     cout<<endl;
-     cout<<"Tidak dapat terdefinisikan karena nilai tidak sesuai"<<endl;
-     cout<<endl;
- }
 
 	//lanjutan untuk kepentingan menampung nilai
-	int kminxn = 1, nbminkn = 1, nkminxn = 1, nbminknkminxn = 1, nbminnkzn = 1;
+	  long long int kminxn = 1, nbminkn = 1, nkminxn = 1, nbminknkminxn = 1, nbminnkzn = 1;
 	        
     //faktorial untuk nbesar, nkecil, k, x 
       for(i=1;i<=nbesar;i++){    
@@ -84,26 +77,31 @@
 	  
 	//catatan : ^ berarti berada di atas dan v artinya berada di bawah
 	//di (^k vn)!, kalikan x! dengan sisa bagi x untuk memenuhi nilai k tadi
-	 long long kiri = kminxn * xn;
+	 long long int kiri = kminxn * xn;
 	//di (^ N - K v n - x), kalikan hasil (n - x)! dengan sisa bagi (n - x)! untuk memenuhi sisa hasil (N - k)!
-	 long long kanan = nkminxn * nbminknkminxn;
+	 long long int kanan = nkminxn * nbminknkminxn;
 	//Ini bagian bawah, kalikan n! dengan sisa bagi n! untuk memenuhi Nilai N!
-	 long long bawah = nbminnkzn * nkn;
+	 long long int bawah = nbminnkzn * nkn;
 	 
-	 long long hasilkiri = kn / kiri;
-	 long long hasilkanan = nbminkn / kanan;
+	 long long int hasilkiri = kn / kiri;
+	 long long int hasilkanan = nbminkn / kanan;
 	 
 	 //kalikan keduanya dari yang telah dihasilkan di atas
-	 long long totalatas = hasilkiri * hasilkanan;
+	 long long int totalatas = hasilkiri * hasilkanan;
 	 
 	 //jumlahkan di bawah
-	 long long totalbawah = nbn / bawah;
+	 long long int totalbawah = nbn / bawah;
 	 
-	 double destotalatas = totalatas, destotalbawah = totalbawah;
-	 double destotalatasbawah = destotalatas / destotalbawah;
-	 double jawaban[1] = {destotalatasbawah};
+	long double destotalatas = totalatas, destotalbawah = totalbawah;
+	long double destotalatasbawah = destotalatas / destotalbawah;
+	long double jawaban[1] = {destotalatasbawah};
 	 
+	 //pemberitahuan mengenai nilai yang kemungkinan terlalu banyak sehingga terhitung secara tidak benar
+     if(x < 1 && k < 1 && nkecil < 1 && nbesar < 1){
+     cout<<endl;
+     cout<<"Tidak dapat terdefinisikan karena nilai tidak sesuai"<<endl;
+     cout<<endl; } else {
 	 kesimpulan();
-	 cout<< totalatas << "/" << totalbawah << " atau " << jawaban[0] << endl;
+	 cout<< totalatas << "/" << totalbawah << " atau " << jawaban[0] << endl; }
       return 0;  
   }
